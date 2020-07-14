0ec0538d4c21e1436404e4660923db5f6a961673
ConstantinEmperor <68290343+ConstantinEmperor@users.noreply.github.com>
2020-07-14 20:52:52 +0300

    Create MyCPPTemplateFIle_main.cpp
    Стандартный шаблон разработки

---
+++ MyCPPTemplateFIle_main.cpp
@@ -0,0 +1,23 @@
+//My_Template^
+#include <iostream>
+
+//-----------------
+//Other defines
+//-----------------
+//Some functions 
+//-----------------
+//Some classes
+//-----------------
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+
+    std::cout << "Hello World!\n";
+
+
+
+	return 0;
+}
+//-----------------
+//realizing
+//-----------------
